a3504269e8920349ee99db34a1f3f3931bf99cc9
ekgns33 <ekgns00421@gmail.com>
2023-10-30 16:48:02 +0900

    feat: Dao 생성, schema.sql생성

---
+++ src/main/java/gdsc/shine/springlearningsimple/dao/HelloDao.java
@@ -0,0 +1,24 @@
+package gdsc.shine.springlearningsimple.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class HelloDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public HelloDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void insertUserName(String userName) {
+        String sql = "insert into USERS (NAME) values (?)";
+        jdbcTemplate.update(sql, userName);
+    }
+    public Optional<Integer> countByUserName(String userName) {
+        String sql = "select COUNT(*) from USERS where NAME = (?)";
+        return Optional.ofNullable(jdbcTemplate.queryForObject(sql, Integer.TYPE, userName));
+    }
+}
