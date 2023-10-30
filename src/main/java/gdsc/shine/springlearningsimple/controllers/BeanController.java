32b4142c1ce16fabed0be2e5d0899da22159d271
ekgns33 <ekgns00421@gmail.com>
2023-10-30 16:39:08 +0900

    feat: 빈 컨트롤러 생성, 빈출력 로직 작성

---
+++ src/main/java/gdsc/shine/springlearningsimple/controllers/BeanController.java
@@ -0,0 +1,22 @@
+package gdsc.shine.springlearningsimple.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BeanController {
+    private final ApplicationContext ac;
+    @Autowired
+    public BeanController(ApplicationContext ac) {
+        this.ac = ac;
+    }
+    @GetMapping("/beans")
+    public List<String> getBeans() {
+        return Arrays.asList(ac.getBeanDefinitionNames());
+    }
+}
