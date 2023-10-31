7158da85479bb5960515b72ac4ef7fa85bd99aa0
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-01 02:32:08 +0900

    feat(schema): drop `users` table before create

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,8 @@
-create table users (
-    id bigint auto_increment,
+drop table if exists users;
+
+create table users
+(
+    id   bigint auto_increment,
     name varchar(255) not null,
     primary key (id)
 )
