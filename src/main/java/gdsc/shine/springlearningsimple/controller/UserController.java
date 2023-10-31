8e5fd79bd6fd6fce38d969ec01e26a34f7e2fe8a
jution113 <jution113@gmail.com>
2023-10-31 13:56:09 +0900

    Feat: implement visited count method in HelloController

--- src/main/java/gdsc/shine/springlearningsimple/controller/UserController.java
+++ src/main/java/gdsc/shine/springlearningsimple/controller/UserController.java
@@ -1,13 +1,29 @@
 package gdsc.shine.springlearningsimple.controller;
 
+import gdsc.shine.springlearningsimple.DAO.HelloDao;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private HelloDao helloDao;
+
+    public UserController(HelloDao helloDao) {
+        this.helloDao = helloDao;
+    }
+
     @GetMapping("/users")
-    public String pringHello(@RequestParam String name) {
-        return "안녕하세요! " + name;
+    public String printHello(@RequestParam String name) {
+        helloDao.insertUserByName(name);
+
+        int visitedCnt =  helloDao.countByUserName(name);
+
+        if(visitedCnt == 1) {
+            return "안녕하세요! " + name;
+        } else {
+            return name + "님 안녕하세요, " + visitedCnt + "번째 방문이시군요!";
+        }
+
     }
 }
