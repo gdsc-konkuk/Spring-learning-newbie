e966d01b42f57de6bd25b9b8ffda17a946b69552
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-01 01:12:37 +0900

    feat(user-controller): add `sayHello` method

--- src/main/java/gdsc/shine/springlearningsimple/domain/user/controller/UserController.java
+++ src/main/java/gdsc/shine/springlearningsimple/domain/user/controller/UserController.java
@@ -1,10 +1,15 @@
 package gdsc.shine.springlearningsimple.domain.user.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-
+	@GetMapping("/users")
+	public String sayHello(@RequestParam String name) {
+		return "안녕하세요 ! " + name;
+	}
 
 }
