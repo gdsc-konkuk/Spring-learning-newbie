c37b54f0e638c73343df96f559bfa960b4bcee29
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-01 09:43:08 +0900

    feat(hello-dao): add @Repository annotation

--- src/main/java/gdsc/shine/springlearningsimple/domain/user/dao/HelloDao.java
+++ src/main/java/gdsc/shine/springlearningsimple/domain/user/dao/HelloDao.java
@@ -1,5 +1,9 @@
 package gdsc.shine.springlearningsimple.domain.user.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class HelloDao {
 
 }
