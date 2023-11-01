6005bf58cb7eafaaa9f9752a8e9a9ef59d687fd7
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-01 12:45:54 +0900

    refactor(user-controller): change HelloDao to UserRepository

--- src/main/java/gdsc/shine/springlearningsimple/domain/user/controller/UserController.java
+++ src/main/java/gdsc/shine/springlearningsimple/domain/user/controller/UserController.java
@@ -4,25 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import gdsc.shine.springlearningsimple.domain.user.dao.HelloDao;
+import gdsc.shine.springlearningsimple.domain.user.repository.UserRepository;
 
 @RestController
 public class UserController {
 
-	private final HelloDao helloDao;
+	private final UserRepository userRepository;
 
-	public UserController(HelloDao helloDao) {
-		this.helloDao = helloDao;
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
 	}
 
 	@GetMapping("/users")
 	public String sayHello(@RequestParam String name) {
-		int visitedCount = helloDao.countByUserName(name);
+		int visitedCount = userRepository.countByUserName(name);
 		if (visitedCount == 0) {
-			helloDao.insertUserByName(name);
+			userRepository.insertUserByName(name);
 			return "안녕하세요 ! " + name;
 		}
-		helloDao.insertUserByName(name);
+		userRepository.insertUserByName(name);
 		return name + "님 안녕하세요, " + visitedCount + "번째 방문이시군요 ! 허허허";
 	}
 
