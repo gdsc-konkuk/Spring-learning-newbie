2c445ffaf7925389844ee5834c22a514cedc94a4
ekgns33 <ekgns00421@gmail.com>
2023-11-01 22:24:35 +0900

    fix: 컨트롤러 의존성 주입 코드 수정

--- src/main/java/gdsc/shine/springlearningsimple/controllers/BeanController.java
+++ src/main/java/gdsc/shine/springlearningsimple/controllers/BeanController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 public class BeanController {
     private final ApplicationContext ac;
-    @Autowired
+
     public BeanController(ApplicationContext ac) {
         this.ac = ac;
     }
--- src/main/java/gdsc/shine/springlearningsimple/controllers/UserController.java
+++ src/main/java/gdsc/shine/springlearningsimple/controllers/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final HelloDao helloDao;
-    @Autowired
+
     public UserController(HelloDao helloDao) {
         this.helloDao = helloDao;
     }
