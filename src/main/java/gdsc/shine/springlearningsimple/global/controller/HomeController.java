80e16be17f657c1545057f941c4495e37c00f615
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-01 13:58:47 +0900

    feat(home-controller): add dependency injection for ApplicationContext

--- src/main/java/gdsc/shine/springlearningsimple/global/controller/HomeController.java
+++ src/main/java/gdsc/shine/springlearningsimple/global/controller/HomeController.java
@@ -1,14 +1,20 @@
 package gdsc.shine.springlearningsimple.global.controller;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+	private final ApplicationContext applicationContext;
+
+	public HomeController(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+
 	@GetMapping("/home")
 	public String greetingWorld() {
 		return "Hello World !!!";
 	}
-
 }
