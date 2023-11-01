751404f01a78ea703da68507b3d869ad6af21c4a
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-01 14:11:29 +0900

    feat(home-controller): add `showAllBeans` method

--- src/main/java/gdsc/shine/springlearningsimple/global/controller/HomeController.java
+++ src/main/java/gdsc/shine/springlearningsimple/global/controller/HomeController.java
@@ -1,5 +1,7 @@
 package gdsc.shine.springlearningsimple.global.controller;
 
+import java.util.Arrays;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,4 +19,15 @@ public class HomeController {
 	public String greetingWorld() {
 		return "Hello World !!!";
 	}
+
+	@GetMapping("/beans")
+	public String showAllBeans() {
+		String[] beans = applicationContext.getBeanDefinitionNames();
+		StringBuilder stringBuilder = new StringBuilder("<pre>");
+		Arrays.stream(beans)
+			.forEach(bean -> stringBuilder.append(bean).append("\n\n"));
+		stringBuilder.append("</pre>");
+		return stringBuilder.toString();
+	}
+
 }
