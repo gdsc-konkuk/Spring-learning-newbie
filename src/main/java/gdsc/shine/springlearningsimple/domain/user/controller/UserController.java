6dd6b113bb3402e5c6f74ded2017810bd2487ec2
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-01 10:26:55 +0900

    feat(user-controller): add dependency injection for HelloDao

--- src/main/java/gdsc/shine/springlearningsimple/domain/user/controller/UserController.java
+++ src/main/java/gdsc/shine/springlearningsimple/domain/user/controller/UserController.java
@@ -4,8 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import gdsc.shine.springlearningsimple.domain.user.dao.HelloDao;
+
 @RestController
 public class UserController {
+
+	private final HelloDao helloDao;
+
+	public UserController(HelloDao helloDao) {
+		this.helloDao = helloDao;
+	}
 
 	@GetMapping("/users")
 	public String sayHello(@RequestParam String name) {
