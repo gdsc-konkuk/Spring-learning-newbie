0fbe97eb7be52441f1770a34ef2c02b85549561c
goldentrash <startoflateness@gmail.com>
2023-11-04 16:51:12 +0900

    [feature] : beans controller가 Background에서 추가된 컨테이너를 제외

--- src/main/java/gdsc/shine/springlearningsimple/controller/BeanController.java
+++ src/main/java/gdsc/shine/springlearningsimple/controller/BeanController.java
@@ -1,22 +1,27 @@
 package gdsc.shine.springlearningsimple.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 public class BeanController {
 
-    private final ApplicationContext ac;
+    private final GenericApplicationContext ac;
 
     @Autowired
-    public BeanController(ApplicationContext ac) {
+    public BeanController(GenericApplicationContext ac) {
         this.ac = ac;
     }
 
     @GetMapping("/beans")
     public String[] getCustomList() {
-        return this.ac.getBeanDefinitionNames();
+        return Arrays.stream(this.ac.getBeanDefinitionNames())
+                .filter(beanName -> this.ac.getBeanDefinition(beanName).getRole() == BeanDefinition.ROLE_APPLICATION)
+                .toArray(String[]::new);
     }
 }
