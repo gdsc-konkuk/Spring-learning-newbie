ff34ce69aaeb9877d6c9b01fcd9c561a0cae37ff
ekgns33 <ekgns00421@gmail.com>
2023-11-04 23:38:20 +0900

    feat: `BeanController.java` modify method to return user Enrolled Beans, remove unused import

--- src/main/java/gdsc/shine/springlearningsimple/controllers/BeanController.java
+++ src/main/java/gdsc/shine/springlearningsimple/controllers/BeanController.java
@@ -1,23 +1,31 @@
 package gdsc.shine.springlearningsimple.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class BeanController {
-    private final ApplicationContext ac;
+    private final AnnotationConfigApplicationContext ac;
 
-    public BeanController(ApplicationContext ac) {
+    public BeanController(AnnotationConfigApplicationContext ac) {
         this.ac = ac;
     }
 
     @GetMapping("/beans")
     public List<String> getBeans() {
-        return Arrays.asList(ac.getBeanDefinitionNames());
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        List<String> userBeans = new ArrayList<>();
+        for(String beanDefinitionName : beanDefinitionNames) {
+             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                 userBeans.add(beanDefinitionName);
+             }
+        }
+        return userBeans;
     }
 }
