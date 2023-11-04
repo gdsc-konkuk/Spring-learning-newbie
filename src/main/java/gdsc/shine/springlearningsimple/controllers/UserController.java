ad30fe70fc38a59591655340a6498839521dba73
ekgns33 <ekgns00421@gmail.com>
2023-11-04 23:39:38 +0900

    refactor: add new-line, apply early return to code

--- src/main/java/gdsc/shine/springlearningsimple/controllers/UserController.java
+++ src/main/java/gdsc/shine/springlearningsimple/controllers/UserController.java
@@ -1,7 +1,6 @@
 package gdsc.shine.springlearningsimple.controllers;
 
 import gdsc.shine.springlearningsimple.dao.HelloDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,9 +22,9 @@ public class UserController {
             int visitCount= helloDao.countByUserName(name).orElse(1);
             if(visitCount == 1) {
                 return "안녕하세요!" + name;
-            } else {
-                return name + "님 안녕하세요, " + (visitCount) + "번째 방문이시군요.";
             }
+            return name + "님 안녕하세요, " + (visitCount) + "번째 방문이시군요.";
+
         } catch (Exception exception) {
             return "서버 에러";
         }
@@ -35,6 +34,7 @@ public class UserController {
     public void joinUser(@RequestParam("name") String name) {
         this.helloDao.insertUserName(name);
     }
+
     @GetMapping("/users/cnt")
     public int cntUser(@RequestParam("name") String name) {
         return this.helloDao.countByUserName(name).orElse(0);
