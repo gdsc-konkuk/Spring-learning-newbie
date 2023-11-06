fcaede63e8d4f39b88f171acc8688757bc59135f
jution113 <jution113@gmail.com>
2023-11-06 11:51:34 +0900

    Feat: MyHour 클래스에 필드 및 생성자 추가

--- src/main/java/gdsc/shine/springlearningsimple/bean/oop/MyHour.java
+++ src/main/java/gdsc/shine/springlearningsimple/bean/oop/MyHour.java
@@ -1,4 +1,14 @@
 package gdsc.shine.springlearningsimple.bean.oop;
 
 public class MyHour {
+    private int hour;
+
+    public MyHour() {
+
+    }
+
+    public MyHour(int hour) {
+        this.hour = hour;
+    }
+
 }
