6e7020ecef4a6a8640b0f6bb6a4991c1da4ed61f
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-10 00:40:32 +0900

    feat(StationConstructorService): add constructor

--- src/main/java/gdsc/shine/springlearningsimple/bean/di/StationConstructorService.java
+++ src/main/java/gdsc/shine/springlearningsimple/bean/di/StationConstructorService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Component;
 public class StationConstructorService {
 	private final StationRepository stationRepository;
 
+	public StationConstructorService(StationRepository stationRepository) {
+		this.stationRepository = stationRepository;
+	}
 
     public String sayHi() {
         return stationRepository.sayHi();
