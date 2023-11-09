916540293bf26685f5227336dd454872d19bbc4e
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-10 00:36:25 +0900

    feat(StationConstructorService): add `@Component` annotation

--- src/main/java/gdsc/shine/springlearningsimple/bean/di/StationConstructorService.java
+++ src/main/java/gdsc/shine/springlearningsimple/bean/di/StationConstructorService.java
@@ -1,5 +1,8 @@
 package gdsc.shine.springlearningsimple.bean.di;
 
+import org.springframework.stereotype.Component;
+
+@Component("stationConstructorService")
 public class StationConstructorService {
     private StationRepository stationRepository;
 
