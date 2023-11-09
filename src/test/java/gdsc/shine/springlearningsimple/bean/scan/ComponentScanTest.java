566609a858f1cbb1895e41247c842f71bf03e817
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-09 23:18:58 +0900

    style(ComponentScanTest): improve code styling

--- src/test/java/gdsc/shine/springlearningsimple/bean/scan/ComponentScanTest.java
+++ src/test/java/gdsc/shine/springlearningsimple/bean/scan/ComponentScanTest.java
@@ -1,36 +1,37 @@
 package gdsc.shine.springlearningsimple.bean.scan;
 
-import gdsc.shine.springlearningsimple.SpringLearningSimpleApplication;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.Arrays;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import gdsc.shine.springlearningsimple.SpringLearningSimpleApplication;
 
 public class ComponentScanTest {
 
-    /**
-     * TODO: core > scan 패키지 내에 있는 클래스를 스프링 빈으로 등록하기
-     */
-    @Test
-    void componentScan() {
-        ApplicationContext context = getApplicationContext();
-        LineDao dao = context.getBean("lineDao", LineDao.class);
-        assertThat(dao).isNotNull();
+	/**
+	 * TODO: bean > scan 패키지 내에 있는 클래스를 스프링 빈으로 등록하기
+	 */
+	@Test
+	void componentScan() {
+		ApplicationContext context = getApplicationContext();
+		LineDao dao = context.getBean("lineDao", LineDao.class);
+		assertThat(dao).isNotNull();
 
-        LineService service = context.getBean("lineService", LineService.class);
-        assertThat(service).isNotNull();
-    }
+		LineService service = context.getBean("lineService", LineService.class);
+		assertThat(service).isNotNull();
+	}
 
-    /**
-     * HelloApplication > @SpringBootApplication 설정을 통해 이미 ComponentScan 설정되어있음
-     */
-    private ApplicationContext getApplicationContext() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(SpringLearningSimpleApplication.class);
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-        System.out.println(Arrays.toString(beanDefinitionNames));
-        return context;
-    }
+	/**
+	 * SpringLearningSimpleApplication > @SpringBootApplication 설정을 통해 이미 ComponentScan 설정되어있음
+	 */
+	private ApplicationContext getApplicationContext() {
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringLearningSimpleApplication.class);
+		String[] beanDefinitionNames = context.getBeanDefinitionNames();
+		System.out.println(Arrays.toString(beanDefinitionNames));
+		return context;
+	}
 }
