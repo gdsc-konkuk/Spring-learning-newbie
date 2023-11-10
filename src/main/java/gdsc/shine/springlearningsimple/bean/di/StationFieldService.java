db50ad759c158359a1e168aca59e0457656d0fa6
ekgns33 <ekgns00421@gmail.com>
2023-11-10 22:25:19 +0900

    feat: StationFieldService, @Autowired를 이용한 필드주입

--- src/main/java/gdsc/shine/springlearningsimple/bean/di/StationFieldService.java
+++ src/main/java/gdsc/shine/springlearningsimple/bean/di/StationFieldService.java
@@ -1,7 +1,14 @@
 package gdsc.shine.springlearningsimple.bean.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class StationFieldService {
+    @Autowired
     private StationRepository stationRepository;
+
+    public StationFieldService() {}
 
     public String sayHi() {
         return stationRepository.sayHi();
