a32fd5f56bd570a55bcd01ec5ce67224927200c7
ekgns33 <ekgns00421@gmail.com>
2023-11-13 15:00:17 +0900

    feat : HttpMethodController에 각각 RequestMapping추가로 요청 처리 분기

--- src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/HttpMethodController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/HttpMethodController.java
@@ -2,21 +2,24 @@ package gdsc.shine.springlearningsimple.mvc.mapping;
 
 import gdsc.shine.springlearningsimple.mvc.domain.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
-@RestController
+@RestController()
+@RequestMapping("/http-method")
 public class HttpMethodController {
 
+    // 어떤 요청을 받을지 명시가 안되어있음.
+    @PostMapping("/users")
     public ResponseEntity createUser(@RequestBody User user) {
         Long id = 1L;
         return ResponseEntity.created(URI.create("/users/" + id)).build();
     }
 
+    @GetMapping("/users")
     public ResponseEntity<List<User>> showUser() {
         List<User> users = Arrays.asList(
                 new User("이름", "email"),
