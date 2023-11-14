ec3fbb31d7e44a3f36269ce5b0ee5e01b77b3aa3
jution113 <jution113@gmail.com>
2023-11-14 16:53:46 +0900

    Feat: add get mehtod request path pathVariable() for testing pathVariable() in UriPatternTest

--- src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/UriPatternController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/UriPatternController.java
@@ -2,6 +2,8 @@ package gdsc.shine.springlearningsimple.mvc.mapping;
 
 import gdsc.shine.springlearningsimple.mvc.domain.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/uri-pattern")
 public class UriPatternController {
 
-    public ResponseEntity<User> pathVariable(Long id) {
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> pathVariable(@PathVariable("id") Long id) {
         User user = new User(id, "이름", "email");
         return ResponseEntity.ok().body(user);
     }
