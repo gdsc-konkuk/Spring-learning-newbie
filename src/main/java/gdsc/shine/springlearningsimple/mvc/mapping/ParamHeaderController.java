f39f5d86b5d262f1f943e648602fedbfedca66d5
jution113 <jution113@gmail.com>
2023-11-14 15:33:18 +0900

    Feat: add get mehtod request path to message() for testing message() { () in ParamHeaderTest

--- src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/ParamHeaderController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/ParamHeaderController.java
@@ -1,6 +1,7 @@
 package gdsc.shine.springlearningsimple.mvc.mapping;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/param-header")
 public class ParamHeaderController {
 
+    @GetMapping("/message")
     public ResponseEntity<String> message() {
         return ResponseEntity.ok().body("message");
     }
