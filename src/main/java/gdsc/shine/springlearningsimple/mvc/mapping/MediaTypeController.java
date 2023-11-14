b80b0213b4cccfd785075cce41d3f54005202e68
jution113 <jution113@gmail.com>
2023-11-14 15:09:46 +0900

    Feat: add post mehtod request path for testing createUser() in MediaTypeTest

--- src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/MediaTypeController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/MediaTypeController.java
@@ -2,6 +2,7 @@ package gdsc.shine.springlearningsimple.mvc.mapping;
 
 import gdsc.shine.springlearningsimple.mvc.domain.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 @RequestMapping("/media-type")
 public class MediaTypeController {
 
+    @PostMapping("/users")
     public ResponseEntity createUser(@RequestBody User user) {
         Long id = 1L;
         return ResponseEntity.created(URI.create("/users/" + id)).build();
