317de834874e97b2e85f0b63db3fba5d84df24ff
ekgns33 <ekgns00421@gmail.com>
2023-11-15 18:49:01 +0900

    feat: CustomException에 생성자 추가

--- src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/exception/CustomException.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/exception/CustomException.java
@@ -2,4 +2,12 @@ package gdsc.shine.springlearningsimple.mvc.exceptions.exception;
 
 public class CustomException extends RuntimeException {
 
+    public CustomException() {
+        super();
+    }
+
+    public CustomException(String message) {
+        super(message);
+    }
+
 }
