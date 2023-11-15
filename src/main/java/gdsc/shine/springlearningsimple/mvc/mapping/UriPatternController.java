a5350321d29f4e32bc82d3795ad37b3ea797d61f
goldentrash <startoflateness@gmail.com>
2023-11-15 15:14:04 +0900

    [test] : UriPattern

--- src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/UriPatternController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/UriPatternController.java
@@ -2,6 +2,8 @@ package gdsc.shine.springlearningsimple.mvc.mapping;
 
 import gdsc.shine.springlearningsimple.mvc.domain.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/uri-pattern")
 public class UriPatternController {
 
-    public ResponseEntity<User> pathVariable(Long id) {
+    @GetMapping("users/{id}")
+    public ResponseEntity<User> pathVariable(@PathVariable Long id) {
         User user = new User(id, "이름", "email");
         return ResponseEntity.ok().body(user);
     }
 
+    /**
+     * When multiple patterns match a URL,
+     * the best match must be selected.
+     * This is done with one of the following depending on
+     * whether use of parsed PathPattern is enabled for use or not:
+     * > PathPattern.SPECIFICITY_COMPARATOR
+     */
+    @GetMapping("patterns/?")
     public ResponseEntity<String> pattern() {
         return ResponseEntity.ok().body("pattern");
     }
 
+    /**
+     * The default mapping pattern (/**) is excluded
+     * from scoring and always sorted last.
+     * Also, prefix patterns (such as /public/**) are
+     * considered less specific than other pattern
+     * that do not have double wildcards.
+     */
+    @GetMapping("patterns/**")
     public ResponseEntity<String> patternStars() {
         return ResponseEntity.ok().body("pattern-multi");
     }
