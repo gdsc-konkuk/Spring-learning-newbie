fad2c741ba31186b8a83f6b6c15aed43d6de9205
ekgns33 <ekgns00421@gmail.com>
2023-11-15 18:49:18 +0900

    feat: HelloException에 생성자 추가

--- src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/exception/HelloException.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/exception/HelloException.java
@@ -2,4 +2,12 @@ package gdsc.shine.springlearningsimple.mvc.exceptions.exception;
 
 public class HelloException extends RuntimeException {
 
+    public HelloException() {
+        super();
+    }
+
+    public HelloException(String message) {
+        super(message);
+    }
+
 }
