2e6cfaa8304b2973efd93ac24e7fd8b5f7efef6c
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-16 23:45:12 +0900

    style(ExceptionsController): reformat code for consistent indentation

--- src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/ExceptionsController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/ExceptionsController.java
@@ -1,27 +1,28 @@
 package gdsc.shine.springlearningsimple.mvc.exceptions;
 
-import gdsc.shine.springlearningsimple.mvc.exceptions.exception.CustomException;
-import gdsc.shine.springlearningsimple.mvc.exceptions.exception.HelloException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import gdsc.shine.springlearningsimple.mvc.exceptions.exception.CustomException;
+import gdsc.shine.springlearningsimple.mvc.exceptions.exception.HelloException;
+
 @RestController
 @RequestMapping("/exceptions")
 public class ExceptionsController {
 
-    @GetMapping("/hello")
-    public ResponseEntity exceptionHandler() {
-        throw new CustomException();
-    }
+	@GetMapping("/hello")
+	public ResponseEntity exceptionHandler() {
+		throw new CustomException();
+	}
 
-    @GetMapping("/hi")
-    public ResponseEntity exceptionHandler2() {
-        throw new HelloException();
-    }
+	@GetMapping("/hi")
+	public ResponseEntity exceptionHandler2() {
+		throw new HelloException();
+	}
 
-    public ResponseEntity<String> handle() {
-        return ResponseEntity.badRequest().body("CustomException");
-    }
+	public ResponseEntity<String> handle() {
+		return ResponseEntity.badRequest().body("CustomException");
+	}
 }
