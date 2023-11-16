f100aac2fbc2c6b1974a3c2551d92c561adfbdbb
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-16 19:37:03 +0900

    refactor(MethodArgumentController): add `@RequestBody` annotation to map `User` instance

--- src/main/java/gdsc/shine/springlearningsimple/mvc/handler/MethodArgumentController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/handler/MethodArgumentController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class MethodArgumentController {
 	}
 
 	@PostMapping("/users/body")
-	public ResponseEntity requestBody(User user) {
+	public ResponseEntity requestBody(@RequestBody User user) {
 		User newUser = new User(1L, user.getName(), user.getEmail());
 		return ResponseEntity.created(URI.create("/users/" + newUser.getId())).body(newUser);
 	}
