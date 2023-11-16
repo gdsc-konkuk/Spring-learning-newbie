bdf9ea3ead947245864e3e58d033a5bf630ce17d
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-16 12:37:10 +0900

    feat(HttpMethodController): add `@GetMapping` annotation

--- src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/HttpMethodController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/HttpMethodController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ public class HttpMethodController {
 		return ResponseEntity.created(URI.create("/users/" + id)).build();
 	}
 
+	@GetMapping("/users")
 	public ResponseEntity<List<User>> showUser() {
 		List<User> users = Arrays.asList(
 			new User("이름", "email"),
