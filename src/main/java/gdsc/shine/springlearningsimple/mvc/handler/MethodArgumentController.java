b99cd3379e8ad0c3c16bcc6b842a02bfcb8d3188
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-16 19:16:24 +0900

    feat(MethodArgumentController): add `@RequestParam` annotation for mapping parameter

--- src/main/java/gdsc/shine/springlearningsimple/mvc/handler/MethodArgumentController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/handler/MethodArgumentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import gdsc.shine.springlearningsimple.mvc.domain.User;
@@ -17,7 +18,7 @@ import gdsc.shine.springlearningsimple.mvc.domain.User;
 public class MethodArgumentController {
 
 	@GetMapping("/users")
-	public ResponseEntity<List<User>> requestParam(String userName) {
+	public ResponseEntity<List<User>> requestParam(@RequestParam String name) {
 		List<User> users = Arrays.asList(
 			new User(userName, "email"),
 			new User(userName, "email")
