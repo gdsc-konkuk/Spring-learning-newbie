010dd40102fe275ebdd52b02cafb7556b8792397
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-16 20:35:19 +0900

    feat(ReturnValueController): add `@GetMapping` and `@ResponseBody`annotation

--- src/main/java/gdsc/shine/springlearningsimple/mvc/handler/ReturnValueController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/handler/ReturnValueController.java
@@ -1,29 +1,33 @@
 package gdsc.shine.springlearningsimple.mvc.handler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/return-value")
 public class ReturnValueController {
 
-//    public void string() {
-//        return "message";
-//    }
-//
-//    public void responseBodyForUser() {
-//        return new User("name", "email");
-//    }
-//
-//    public void responseEntity(@PathVariable Long id) {
-//        return ResponseEntity.ok(new User("name", "email"));
-//    }
-//
-//    public void responseEntityFor400() {
-//        return ResponseEntity.badRequest().build();
-//    }
-//
-//    public void thymeleaf() {
-//        return "sample";
-//    }
+	@GetMapping("/message")
+	@ResponseBody
+	public String string() {
+		return "message";
+	}
+
+	// public void responseBodyForUser() {
+	// 	return new User("name", "email");
+	// }
+	//
+	// public void responseEntity(@PathVariable Long id) {
+	// 	return ResponseEntity.ok(new User("name", "email"));
+	// }
+	//
+	// public void responseEntityFor400() {
+	// 	return ResponseEntity.badRequest().build();
+	// }
+	//
+	// public void thymeleaf() {
+	// 	return "sample";
+	// }
 }
