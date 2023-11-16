0bf0e5e044520d2a03eb1d1c74c7a2de2e1cd673
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-16 23:58:56 +0900

    feat(HelloAdvice): add `@ExceptionHandler` annotation on `handle`method

--- src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/HelloAdvice.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/HelloAdvice.java
@@ -1,10 +1,15 @@
 package gdsc.shine.springlearningsimple.mvc.exceptions;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import gdsc.shine.springlearningsimple.mvc.exceptions.exception.HelloException;
 
 @RestControllerAdvice
 public class HelloAdvice {
+
+	@ExceptionHandler(HelloException.class)
 	public ResponseEntity<String> handle() {
 		return ResponseEntity.badRequest().body("HelloException");
 	}
