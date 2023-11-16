36bc161f644a1c6fccc4ab4baf680087814690a6
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-16 20:48:29 +0900

    feat(ReturnValueController): add `@GetMapping` and `@ResponseBody` annotation on `responseBodyForUser` method

--- src/main/java/gdsc/shine/springlearningsimple/mvc/handler/ReturnValueController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/handler/ReturnValueController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import gdsc.shine.springlearningsimple.mvc.domain.User;
+
 @Controller
 @RequestMapping("/return-value")
 public class ReturnValueController {
@@ -15,10 +17,12 @@ public class ReturnValueController {
 		return "message";
 	}
 
-	// public void responseBodyForUser() {
-	// 	return new User("name", "email");
-	// }
-	//
+	@GetMapping("/users")
+	@ResponseBody
+	public User responseBodyForUser() {
+		return new User("name", "email");
+	}
+
 	// public void responseEntity(@PathVariable Long id) {
 	// 	return ResponseEntity.ok(new User("name", "email"));
 	// }
