723f18e4d3c4b9830ee8c678cd7b81cdb064ddc3
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-16 21:20:28 +0900

    feat(ReturnValueController): add `@GetMapping` annotation on `responseEntity` method with `@PathVariable`

--- src/main/java/gdsc/shine/springlearningsimple/mvc/handler/ReturnValueController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/handler/ReturnValueController.java
@@ -1,7 +1,9 @@
 package gdsc.shine.springlearningsimple.mvc.handler;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,10 +25,11 @@ public class ReturnValueController {
 		return new User("name", "email");
 	}
 
-	// public void responseEntity(@PathVariable Long id) {
-	// 	return ResponseEntity.ok(new User("name", "email"));
-	// }
-	//
+	@GetMapping("/users/{id}")
+	public ResponseEntity responseEntity(@PathVariable Long id) {
+		return ResponseEntity.ok(new User("name", "email"));
+	}
+
 	// public void responseEntityFor400() {
 	// 	return ResponseEntity.badRequest().build();
 	// }
