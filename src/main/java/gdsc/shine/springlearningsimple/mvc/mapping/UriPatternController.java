d957c9eeb6d0eee8aa41cb528be9da8c8f65432f
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-16 18:33:33 +0900

    feat(UriPatternController): add `@GetMapping` annotation with wild card

--- src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/UriPatternController.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/mapping/UriPatternController.java
@@ -18,6 +18,7 @@ public class UriPatternController {
 		return ResponseEntity.ok().body(user);
 	}
 
+	@GetMapping("/patterns/*")
 	public ResponseEntity<String> pattern() {
 		return ResponseEntity.ok().body("pattern");
 	}
