e82e23fc6aea5b439c1b48c7b94f6640724518a7
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-16 23:58:33 +0900

    feat(HelloAdvice): add `@RestControllerAdvice` annotation on `HelloAdvice` class

--- src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/HelloAdvice.java
+++ src/main/java/gdsc/shine/springlearningsimple/mvc/exceptions/HelloAdvice.java
@@ -1,7 +1,9 @@
 package gdsc.shine.springlearningsimple.mvc.exceptions;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class HelloAdvice {
 	public ResponseEntity<String> handle() {
 		return ResponseEntity.badRequest().body("HelloException");
