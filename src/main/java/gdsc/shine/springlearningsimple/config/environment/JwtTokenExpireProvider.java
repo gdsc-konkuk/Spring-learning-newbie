be11d39ff133ec86167e9be78acbfd409dae5f73
jution113 <jution113@gmail.com>
2023-11-30 00:01:55 +0900

    Feat: add default constructor and component, value annotation

--- src/main/java/gdsc/shine/springlearningsimple/config/environment/JwtTokenExpireProvider.java
+++ src/main/java/gdsc/shine/springlearningsimple/config/environment/JwtTokenExpireProvider.java
@@ -1,9 +1,16 @@
 package gdsc.shine.springlearningsimple.config.environment;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 // TODO: 컴포넌트 스캔을 통한 빈 등록
+@Component
 public class JwtTokenExpireProvider {
     // TODO: application.properties의 security.jwt.token.expire-length 값을 활용하여 validityInMilliseconds값 초기화 하기
+    @Value("${security.jwt.token.expire-length}")
     private long validityInMilliseconds;
+
+    public JwtTokenExpireProvider() {}
 
     public JwtTokenExpireProvider(long validityInMilliseconds) {
         this.validityInMilliseconds = validityInMilliseconds;
