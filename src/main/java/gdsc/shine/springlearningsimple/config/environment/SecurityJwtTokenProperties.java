a74e42d4a87c0ba0075bcb212060e743cada7381
ekgns33 <ekgns00421@gmail.com>
2023-11-30 11:48:37 +0900

    feat: ConfigurationProperties로 등록하고, application-test.properties에서 값 가져오기

--- src/main/java/gdsc/shine/springlearningsimple/config/environment/SecurityJwtTokenProperties.java
+++ src/main/java/gdsc/shine/springlearningsimple/config/environment/SecurityJwtTokenProperties.java
@@ -2,6 +2,7 @@ package gdsc.shine.springlearningsimple.config.environment;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 /**
