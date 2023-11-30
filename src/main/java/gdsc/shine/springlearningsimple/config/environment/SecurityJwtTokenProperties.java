f3a3eddfa9bc4fb783637e7e6215ccd95248fb92
ekgns33 <ekgns00421@gmail.com>
2023-11-30 11:49:04 +0900

    refactor: 사용되지 않는 import문 제거

--- src/main/java/gdsc/shine/springlearningsimple/config/environment/SecurityJwtTokenProperties.java
+++ src/main/java/gdsc/shine/springlearningsimple/config/environment/SecurityJwtTokenProperties.java
@@ -2,7 +2,6 @@ package gdsc.shine.springlearningsimple.config.environment;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 /**
