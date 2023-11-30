6ed7e3f1bed89df33b27a1a5711a7d175d6d3471
ekgns33 <ekgns00421@gmail.com>
2023-11-30 11:31:41 +0900

    feat: ConfigurationProperties 어노테이션 추가, 환경변수값 주입

--- src/main/java/gdsc/shine/springlearningsimple/config/environment/SecurityJwtTokenProperties.java
+++ src/main/java/gdsc/shine/springlearningsimple/config/environment/SecurityJwtTokenProperties.java
@@ -1,16 +1,22 @@
 package gdsc.shine.springlearningsimple.config.environment;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+
 /**
  * ConfigurationPropertiesTest 에서 사용 중
  * ConfigurationPropertiesTest 에서 ConfigurationPropertiesScan 을 선언해주었기 때문에 컴파일 에러 무시 가능!
  *
  * setter를 추가하지 말고 동작하도록~!
  */
+@ConfigurationProperties
+@PropertySource("classpath:application-test.properties")
 public class SecurityJwtTokenProperties {
     private String secretKey;
     private String expireLength;
 
-    public SecurityJwtTokenProperties(String secretKey, String expireLength) {
+    public SecurityJwtTokenProperties(@Value("${security.jwt.token.secret-key}")String secretKey, @Value("${security.jwt.token.expire-length}")String expireLength) {
         this.secretKey = secretKey;
         this.expireLength = expireLength;
     }
