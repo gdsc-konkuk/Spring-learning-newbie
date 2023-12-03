07be259eae01c5a0975a91ea343feb620ddb628e
goldentrash <startoflateness@gmail.com>
2023-12-03 20:00:46 +0900

    [test] : fix test

--- src/main/java/gdsc/shine/springlearningsimple/config/environment/SecurityJwtTokenProperties.java
+++ src/main/java/gdsc/shine/springlearningsimple/config/environment/SecurityJwtTokenProperties.java
@@ -1,11 +1,16 @@
 package gdsc.shine.springlearningsimple.config.environment;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
 /**
  * ConfigurationPropertiesTest 에서 사용 중
  * ConfigurationPropertiesTest 에서 ConfigurationPropertiesScan 을 선언해주었기 때문에 컴파일 에러 무시 가능!
- *
+ * <p>
  * setter를 추가하지 말고 동작하도록~!
  */
+@ConfigurationProperties(prefix = "security.jwt.token")
+@ConstructorBinding
 public class SecurityJwtTokenProperties {
     private String secretKey;
     private String expireLength;
